65a41aba956f25764966afa6473744dd9ed116e4
ghkdtnqls95 <ghkdtnqls95@gmail.com>
2024-07-05 10:30:24 +0900

    add ch01

---
+++ ch01/src/sub1/Hello.java
@@ -0,0 +1,14 @@
+package sub1;
+/*
+ * 날짜: 2024/06/28
+ * 이름: 김소진
+ * 내용: ch01.java 개요 및 개발환경 구축
+ * 
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+	}
+}
---
+++ ch01/src/sub2/PrintTest.java
@@ -0,0 +1,26 @@
+package sub2;
+
+public class PrintTest {
+/*
+ * 날짜: 2024/06/28
+ * 이름: 김소진
+ * 내용: 자바 출력 실습하기
+ */
+	public static void main(String[] args) {
+		//기본출력
+		System.out.println("Hello java!");
+		System.out.println("Hello Korea!");
+		System.out.println("헤이 모두들 안녕~");
+		//한줄출력
+		//ln: line 줄바꿈
+		//ln 처리가 안되어서 그다음 출력문이 이어서 출력
+		System.out.print("Welcome");
+		System.out.print(" Java쓰~~");
+		//개행처리
+		System.out.println();
+		System.out.print("Welcome~");
+		System.out.print("Korea\n");  //줄바꿈(개행)처리
+		System.out.print("Korea~");
+	}
+
+}
