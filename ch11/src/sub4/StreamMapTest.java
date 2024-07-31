c338afc2f53ec63fefc1e965aefc4ef9f230444f
subin3578 <ghkdtnqls95@gmail.com>
2024-07-31 17:53:29 +0900

    ch11

--- ch11/src/sub4/StreamMapTest.java
+++ ch11/src/sub4/StreamMapTest.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-class Person{
+class Person {
 	private String name;
 	private int age;
 	
@@ -18,13 +18,9 @@ class Person{
 		this.name = name;
 		this.age = age;
 	}
-
+	
 	public String getName() {
 		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	@Override
@@ -33,41 +29,43 @@ class Person{
 	}
 }
 
-
 public class StreamMapTest {
 	public static void main(String[] args) {
-		
+		// Person 리스트 생성
 		List<Person> people = new ArrayList<>();
-		people.add(new Person ("김유신",23));
-		people.add(new Person ("김춘추",21));
-		people.add(new Person ("장보고",33));
-		people.add(new Person ("강감찬",43));
-		people.add(new Person ("이순신",53));
-		
+		people.add(new Person("김유신", 23));
+		people.add(new Person("김춘추", 21));
+		people.add(new Person("장보고", 33));
+		people.add(new Person("강감찬", 43));
+		people.add(new Person("이순신", 53));
 		
 		// map 스트림
 		List<String> names = people.stream()
-				  					.map((person)->{
-				  						return person.getName(); //새로운 데이터로 다음 스트림에 전달
-				  						})
-				  					.collect(Collectors.toList());
-
+								   .map((person) -> {
+									  return person.getName();	// 새로운 데이터 변환해서 다음 스트림으로 전달
+								   })
+								   .collect(Collectors.toList()); // 스트림으로 전송된 데이터 취합해 컬렉션으로 반환
+		
 		System.out.println(names);
+		
+		
 		// flatMap 스트림
-	
-		List<E> lists = List.of(
-			ㄴㄴㄴ				Arrays.asList("apple","banana"),
-							Arrays.asList("cherry","grape"),
-							Arrays.asList("orange","mango")
-							);
+		List<List<String>> lists = List.of(
+										Arrays.asList("apple", "banana"),
+										Arrays.asList("cherry", "grape"),
+										Arrays.asList("orange", "mango")
+									);
+		
 		System.out.println(lists);
-
+		
 		List<String> fruits = lists.stream()
-									.flatMap((list)->{
-										return list.stream();
-									})
-									.collect(Collectors.toList());
+								  .flatMap((list)->{
+									  return list.stream();
+								  })
+								  .collect(Collectors.toList());
+		
 		
 		System.out.println(fruits);
+		
 	}
 }
